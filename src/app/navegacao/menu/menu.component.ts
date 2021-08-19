222aa72c809944ef22eb7520a8cdc51c4f9290e9
Jorge Ferreira <sollar777@gmail.com>
2021-08-19 20:50:44 -0300

    iniciando projeto front-end

---
+++ src/app/navegacao/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './menu.component.html'
+})
+export class MenuComponent {
+
+    public isCollapsed: boolean;
+
+    constructor() {
+        this.isCollapsed = true;
+    }
+
+}
---
+++ src/app/navegacao/not-found/not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-not-fount',
+    templateUrl: './not-found.component.html'
+})
+export class NotfoundComponent {}
