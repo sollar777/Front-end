f78d6a924b7e3b204a255f29c747bd0674be3f9c
Jorge Ferreira <sollar777@gmail.com>
2021-08-21 17:54:18 -0300

    form cadastro de usuario com validacao

--- src/app/conta/cadastro/cadastro.component.ts
+++ src/app/conta/cadastro/cadastro.component.ts
@@ -1,14 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'ngx-custom-validators';
+import { fromEvent, merge, Observable } from 'rxjs';
+import { DisplayMessage, GenericValidator, ValidationMessages } from 'src/app/utils/generic-form-validations';
+import { Usuario } from '../models/usuario';
+import { ContaService } from '../services/conta.service';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html'
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChildren(FormControlName, { read: ElementRef }) formInputElements!: ElementRef[];
+
+  errors: any[] = [];
+  cadastroForm!: FormGroup;
+  usuario!: Usuario;
+
+  validationMessages!: ValidationMessages;
+  genericValidation!: GenericValidator;
+  displayMessage: DisplayMessage = {};
+
+  constructor(private fb: FormBuilder, private contaService: ContaService) { 
+
+    this.validationMessages = {
+      email: {
+        required: 'Informe seu e-mail',
+        email: 'Email inválido'
+      },
+      password: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      confirmPassword: {
+        required: 'Informe a senha novamente',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    };
+
+    this.genericValidation = new GenericValidator(this.validationMessages);
+  }
 
   ngOnInit(): void {
+
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
+    let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15]), CustomValidators.equalTo(senha)]);
+
+    this.cadastroForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: senha,
+      confirmPassword: senhaConfirm
+    });
+  }
+
+  ngAfterViewInit(): void {
+
+    let controlBlurs: Observable<any>[] = this.formInputElements
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+      merge(...controlBlurs).subscribe(() => {
+        this.displayMessage = this.genericValidation.processarMensagens(this.cadastroForm);
+      });
+
+  }
+
+  adicionarConta(){
+    if (this.cadastroForm.dirty && this.cadastroForm.valid){
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+
+      this.contaService.registrarUsuario(this.usuario);
+    }
   }
 
 }
